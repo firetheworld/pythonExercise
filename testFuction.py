730a3f8556e53b104838531875570e6eef0de33a
Flame <firetheworld@hotmail.com>
2016-04-19 10:34:45 +0800

    copy code to testFunction

---
+++ testFuction.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from myFunction import *
+
+# print(my_abs(-12))
+
+# x, y = move(100, 100, 60, math.pi / 6)
+# print(x, y)
+
+# numberList = [1, 20, -23, 234, 3458973,-1231231]
+# print(findMax(numberList))
+
+
+name = input('Pls input your name: ')
+print('hello, ',name)
