48e1ada2c8d5b7756823a902d443ad1fe6a6720c
firetheworld <firetheworld@hotmail.com>
2016-05-08 15:59:13 +0800

    test map/reduce

--- testFuction.py
+++ testFuction.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from myFunction import *
+from functools import reduce
 
 # print(my_abs(-12))
 
@@ -11,5 +12,25 @@ from myFunction import *
 # print(findMax(numberList))
 
 
-name = input('Please input your name: ')
-print('hello, ',name)
+# name = input('Please input your name: ')
+# print('hello, ',name)
+
+
+# r = map(power,[1, 2, 3, 4])
+# print(list(r))
+
+
+def add(x, y):
+    return x * 10 + y
+
+r = reduce(add, [1, 2 ,3])
+print(r)
+
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+    def char2num(s):
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+    return reduce(fn, map(char2num, s))
+
+print(str2int('123456'))
